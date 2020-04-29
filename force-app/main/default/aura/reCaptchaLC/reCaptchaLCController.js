ac2375f3fb618efca2e7911395182be71109ccbd
Unknown <blacklux_carch@hotmail.com>
2020-04-29 16:41:03 -0500

    Controller now receives values the user entered in the Case Form.

--- force-app/main/default/aura/reCaptchaLC/reCaptchaLCController.js
+++ force-app/main/default/aura/reCaptchaLC/reCaptchaLCController.js
@@ -19,6 +19,31 @@
     },
 
     doSubmit : function(component, event, helper) {
-        alert("Do Submit"); // TO-DO CALL APEX CODE NOT BUILT YET
+
+        // Get Contact Attributes from Form
+        
+        let firstName = component.get("v.newContact.FirstName");
+        let lastName = component.get("v.newContact.LastName");
+        let secondLastName = component.get("v.newContact.SecondLastName");
+        let email = component.get("v.newContact.Email");
+        let companyName = component.get("v.newContact.CompanyName");
+        let phoneNumber = component.get("v.newContact.PhoneNumber");
+
+        // Get Case Attributes from Form
+        let caseSubject = component.get("v.newCase.Subject");
+        let caseSelectedType = component.find("caseType").get("v.value"); // Find using the aura:id identifier
+        let caseDescription = component.get("v.newCase.Description");
+
+        alert("FN: " + firstName + "\n" + 
+              "LN: " + lastName + "\n" +
+              "SLN: " + secondLastName + "\n" +
+              "EM: " + email + "\n" +
+              "CN: " + companyName + "\n" +
+              "PN: " + phoneNumber + "\n" +
+              "CS: " + caseSubject + "\n" +
+              "Type of case? : " + caseSelectedType + "\n" +
+              "CD: " + caseDescription
+            );           
+        // TO-DO CALL APEX CODE NOT BUILT YET
     }
 })
